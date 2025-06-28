e7e94a7bf99ed2ec5a9c78996536970df5def43c
ayudash <adrianyudhaswara@gmail.com>
2025-06-28 16:06:06 +0700

    update sql

--- koperasi_rbb.sql
+++ koperasi_rbb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 27, 2025 at 08:21 AM
+-- Generation Time: Jun 28, 2025 at 11:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,8 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `koperasi_rbb`
 --
-CREATE DATABASE koperasi_rbb;
-USE koperasi_rbb;
+
 -- --------------------------------------------------------
 
 --
@@ -52,9 +51,9 @@ CREATE TABLE `angsuran` (
   `id_angsuran` int(11) NOT NULL,
   `id_pinjaman` int(11) NOT NULL,
   `id_metode_pembayaran` int(11) DEFAULT NULL,
-  `bulan_ke` int(11) NOT NULL,
+  `angsuran_ke` int(11) NOT NULL,
   `nominal_angsuran` int(9) NOT NULL,
-  `tgl_pembayaran` datetime DEFAULT current_timestamp(),
+  `tgl_pembayaran` datetime DEFAULT NULL,
   `status` enum('LUNAS','BELUM LUNAS') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -62,19 +61,19 @@ CREATE TABLE `angsuran` (
 -- Dumping data for table `angsuran`
 --
 
-INSERT INTO `angsuran` (`id_angsuran`, `id_pinjaman`, `id_metode_pembayaran`, `bulan_ke`, `nominal_angsuran`, `tgl_pembayaran`, `status`) VALUES
-(13, 1, NULL, 1, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(14, 1, NULL, 2, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(15, 1, NULL, 3, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(16, 1, NULL, 4, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(17, 1, NULL, 5, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(18, 1, NULL, 6, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(19, 1, NULL, 7, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(20, 1, NULL, 8, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(21, 1, NULL, 9, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(22, 1, NULL, 10, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(23, 1, NULL, 11, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS'),
-(24, 1, NULL, 12, 16666, '2025-06-27 13:19:54', 'BELUM LUNAS');
+INSERT INTO `angsuran` (`id_angsuran`, `id_pinjaman`, `id_metode_pembayaran`, `angsuran_ke`, `nominal_angsuran`, `tgl_pembayaran`, `status`) VALUES
+(41, 1, NULL, 1, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(42, 1, NULL, 2, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(43, 1, NULL, 3, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(44, 1, NULL, 4, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(45, 1, NULL, 5, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(46, 1, NULL, 6, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(47, 1, NULL, 7, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(48, 1, NULL, 8, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(49, 1, NULL, 9, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(50, 1, NULL, 10, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(51, 1, NULL, 11, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS'),
+(52, 1, NULL, 12, 16666, '2025-06-28 01:17:32', 'BELUM LUNAS');
 
 -- --------------------------------------------------------
 
@@ -127,7 +126,8 @@ CREATE TABLE `nasabah` (
 
 INSERT INTO `nasabah` (`id_nasabah`, `id_jabatan`, `nama_lengkap`, `username`, `hashed_password`, `status`) VALUES
 (1, 1, 'adrian', 'adrian', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 'AKTIF'),
-(2, 1, 'adrian lagi', 'adrian2', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 'AKTIF');
+(2, 1, 'adrian lagi', 'adrian2', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', 'AKTIF'),
+(3, 1, 'siapa ya', 'siapaya', '52c62ec0d3a933be0e9f8f4fa7f3dae1c383bf05b0d068db8a63ab71a387b886', 'AKTIF');
 
 -- --------------------------------------------------------
 
@@ -150,7 +150,8 @@ CREATE TABLE `pinjaman` (
 
 INSERT INTO `pinjaman` (`id_pinjaman`, `id_nasabah`, `nominal_pinjaman`, `tenor`, `alasan_pengajuan`, `tgl_pengajuan`) VALUES
 (1, 1, 200000, 12, 'butuh duit', '2025-06-27 09:14:13'),
-(2, 2, 10000000, 4, ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ligula eros, maximus ac vestibulum sed, rutrum rhoncus arcu. Etiam convallis euismod eros. In vulputate nisl felis, sit amet tempus nulla gravida eu. Fusce in purus a justo pretium congue. Sed id enim id justo pharetra mollis. Nullam ullamcorper elit vitae tortor efficitur gravida. Donec sed malesuada lacus. In tincidunt porta scelerisque. Nam ullamcorper ipsum eu urna sodales efficitur.\r\n\r\nInterdum et malesuada fames ac ante ipsum primis in faucibus. Nam sagittis ut urna a faucibus. Aliquam euismod lacus sed rhoncus mollis. Nullam blandit maximus maximus. Suspendisse potenti. Morbi sit amet semper tellus. Sed scelerisque sem id lacus rhoncus, sed facilisis leo egestas. Sed tempor nulla non arcu semper cursus id at lacus.\r\n\r\n', '2025-06-27 10:23:39');
+(2, 2, 10000000, 4, ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ligula eros, maximus ac vestibulum sed, rutrum rhoncus arcu. Etiam convallis euismod eros. In vulputate nisl felis, sit amet tempus nulla gravida eu. Fusce in purus a justo pretium congue. Sed id enim id justo pharetra mollis. Nullam ullamcorper elit vitae tortor efficitur gravida. Donec sed malesuada lacus. In tincidunt porta scelerisque. Nam ullamcorper ipsum eu urna sodales efficitur.\r\n\r\nInterdum et malesuada fames ac ante ipsum primis in faucibus. Nam sagittis ut urna a faucibus. Aliquam euismod lacus sed rhoncus mollis. Nullam blandit maximus maximus. Suspendisse potenti. Morbi sit amet semper tellus. Sed scelerisque sem id lacus rhoncus, sed facilisis leo egestas. Sed tempor nulla non arcu semper cursus id at lacus.\r\n\r\n', '2025-06-27 10:23:39'),
+(3, 1, 9999999, 12, 'tes', '2025-06-27 14:20:48');
 
 -- --------------------------------------------------------
 
@@ -170,7 +171,11 @@ CREATE TABLE `simpanan` (
 --
 
 INSERT INTO `simpanan` (`id_simpanan`, `id_nasabah`, `nominal_simpanan`, `tgl_uang_masuk`) VALUES
-(1, 1, 100000, '2025-06-26 00:00:00');
+(1, 1, 100000, '2025-06-26 00:00:00'),
+(2, 2, 83000, '2025-06-27 14:13:16'),
+(3, 1, 100000, '2025-06-28 00:00:00'),
+(4, 2, 1000000, '2025-06-11 00:00:00'),
+(5, 3, 1000000, '2025-06-28 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -191,7 +196,8 @@ CREATE TABLE `status_pinjaman` (
 
 INSERT INTO `status_pinjaman` (`id_status_pinjaman`, `id_pinjaman`, `tgl_status`, `status`) VALUES
 (1, 1, '2025-06-27 09:18:15', 'DISETUJUI'),
-(2, 2, '2025-06-27 10:23:53', 'DITOLAK');
+(2, 2, '2025-06-27 10:23:53', 'DITOLAK'),
+(3, 3, '2025-06-27 14:22:24', 'DITOLAK');
 
 --
 -- Indexes for dumped tables
@@ -269,7 +275,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `angsuran`
 --
 ALTER TABLE `angsuran`
-  MODIFY `id_angsuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_angsuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `jabatan`
@@ -287,25 +293,25 @@ ALTER TABLE `metode_pembayaran`
 -- AUTO_INCREMENT for table `nasabah`
 --
 ALTER TABLE `nasabah`
-  MODIFY `id_nasabah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_nasabah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pinjaman`
 --
 ALTER TABLE `pinjaman`
-  MODIFY `id_pinjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pinjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `simpanan`
 --
 ALTER TABLE `simpanan`
-  MODIFY `id_simpanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_simpanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `status_pinjaman`
 --
 ALTER TABLE `status_pinjaman`
-  MODIFY `id_status_pinjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_status_pinjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
