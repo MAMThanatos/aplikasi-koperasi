9e1fa3d1cc07b356c5d6f144dbe89670e88f3be9
ayudash <adrianyudhaswara@gmail.com>
2025-06-29 15:12:13 +0700

    update file DatabaseConnection.java

--- koperasi-rbb/src/utils/DatabaseConnection.java
+++ koperasi-rbb/src/utils/DatabaseConnection.java
@@ -6,26 +6,42 @@
 
 package utils;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 /**
  *
  * @author Aziz
+ * @author wtf
  */
+
 public class DatabaseConnection {
    public static Connection connect() {
         Connection conn = null;
 
         try {
+            Properties prop = new Properties();
+            InputStream input = new FileInputStream("src/config.properties");
+            prop.load(input);
+
+            String host = prop.getProperty("db.host");
+            String port = prop.getProperty("db.port");
+            String db = prop.getProperty("db.name");
+            String user = prop.getProperty("db.user");
+            String pass = prop.getProperty("db.pass");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
+
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/koperasi_rbb", "root", ""
-            );
+            conn = DriverManager.getConnection(url, user, pass);
             System.out.println("Koneksi berhasil");
         } catch (Exception e) {
             System.out.println("Koneksi gagal: " + e.getMessage());
         }
+
         return conn;
-    } 
+    }
 }
