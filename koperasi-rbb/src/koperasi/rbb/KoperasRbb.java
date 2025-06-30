8e433ce7de24c83e2fca85ffe33cbff18762e304
ayudash <adrianyudhaswara@gmail.com>
2025-06-30 19:04:42 +0700

    update file KoperasiRbb.java

--- koperasi-rbb/src/koperasi/rbb/KoperasRbb.java
+++ koperasi-rbb/src/koperasi/rbb/KoperasRbb.java
@@ -1,20 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package koperasi.rbb;
 
-/**
- *
- * @author wtf
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import views.LoginView;
+
 public class KoperasRbb {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (!isDatabaseExist("koperasi_rbb")) {
+            JOptionPane.showMessageDialog(
+                null,
+                "Database 'koperasi_rbb' tidak ditemukan!\nSilakan buat database terlebih dahulu.",
+                "Database Tidak Ditemukan",
+                JOptionPane.ERROR_MESSAGE
+            );
+            System.exit(1);
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            new LoginView().setVisible(true);
+        });
     }
-    
+
+
+    public static boolean isDatabaseExist(String dbName) {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "")) {
+            ResultSet rs = conn.getMetaData().getCatalogs();
+            while (rs.next()) {
+                if (rs.getString(1).equalsIgnoreCase(dbName)) return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
